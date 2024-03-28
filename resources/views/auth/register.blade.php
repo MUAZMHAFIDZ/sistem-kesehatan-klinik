9b8373d33d63f3b307a4f172d98a80a45093bc2d
Bagus Prasetyo <bagusprasetyo@MacBook-Air.local>
2024-03-28 11:24:55 +0700

    membuat tampilan register pasien

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,49 @@
-@extends('layout.navbarPasien')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="/css/pasien/register-pasien.css">
+    
+</head>
+<body>
 
-@section('content')
-        
-    <form method="POST" action="{{ route('register.submit') }}">
-        @csrf
-        <div class="name">
-            <label for="name">UserName</label>
-            <input type="text" name="name" placeholder="UserName" required>
-        </div>
-        <div>
-            <label for="fullname">Full Name</label>
-            <input type="text" name="fullname" placeholder="Full Name" required>
-        </div>
-        <div>
-            <label for="nohp">No HP</label>
-            <input type="number" name="nohp" placeholder="Masukkan No HP" required>
-        </div>
-        <!-- <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" placeholder="Email" required>
-        </div> -->
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="Password" required>
-        </div>
-        <div>
-            <label for="password_confirmation">Password Confirmation</label>
-            <input type="password" name="password_confirmation" placeholder="Password" required autocomplete="new-password">
-        </div>
-        <!-- <div>
-            <label for="photo">Foto Profil</label>
-            <input type="file" name="photo">
-        </div> -->
-        <button type="submit">Daftar</button>
-    </form>
-@endsection
+    @include('pasien.layout.navbarPasien')
+
+        <form method="POST" action="{{ route('register.submit') }}">
+            @csrf
+            <div class="name">
+                <label for="name">UserName</label>
+                <input type="text" name="name" placeholder="UserName" required>
+            </div>
+            <div>
+                <label for="fullname">Full Name</label>
+                <input type="text" name="fullname" placeholder="Full Name" required>
+            </div>
+            <div>
+                <label for="nohp">No HP</label>
+                <input type="number" name="nohp" placeholder="Masukkan No HP" required>
+            </div>
+            <!-- <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" placeholder="Email" required>
+            </div> -->
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <div>
+                <label for="password_confirmation">Password Confirmation</label>
+                <input type="password" name="password_confirmation" placeholder="Password" required autocomplete="new-password">
+            </div>
+            <!-- <div>
+                <label for="photo">Foto Profil</label>
+                <input type="file" name="photo">
+            </div> -->
+            <button type="submit">Daftar</button>
+        </form>
+    
+</body>
+</html>
